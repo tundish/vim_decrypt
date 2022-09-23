c144b4bb6455760cd6462195042329dc6dbb6413
tundish <tundish@gigeconomy.org.uk>
2022-09-23 10:03:30 +0100

    Fix entrypoint.

--- vimdecrypt.py
+++ vimdecrypt.py
@@ -67,7 +67,7 @@ def run():
     if len(sys.argv) > 2:
         sys.exit("usage: vimdecrypt [path]")
 
-    print(vimdecrypt.decrypt(sys.argv[1] if len(sys.argv) == 2 else sys.stdin.buffer))
+    print(decrypt(sys.argv[1] if len(sys.argv) == 2 else sys.stdin.buffer))
 
 
 if __name__ == "__main__":
